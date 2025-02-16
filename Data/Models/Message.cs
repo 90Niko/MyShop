0982e9d6f64ba5475b0449ee58436e799539d546
Nikolay Geroev <ngeroev78@gmail.com>
2025-02-16 11:51:11 +0100

    Add Message Entity

---
+++ Data/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.Data.Models
+{
+    public class Message
+    {
+        [Key]
+        [Comment("PK of Message")]
+        public int Id { get; set; }  // Auto-generated ID
+
+        [Required]
+        [Comment("Sender of Message")]
+        public string Sender { get; set; }=string.Empty;
+
+        [Required]
+        [Comment("Receiver of Message")]
+        public string Content { get; set; }= string.Empty;
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+    }
+}
