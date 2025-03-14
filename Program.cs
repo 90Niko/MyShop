59f58fcc4a9678e1cb896fb76a5a9a8a29acee93
Nikolay Geroev <ngeroev78@gmail.com>
2025-03-14 09:41:22 +0100

    update

--- Program.cs
+++ Program.cs
@@ -154,8 +154,7 @@ namespace MyShop
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                    
-
+                    c.RoutePrefix = "swagger";
                 });
             }
 
